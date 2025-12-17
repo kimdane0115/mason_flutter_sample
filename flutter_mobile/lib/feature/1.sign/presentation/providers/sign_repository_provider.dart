7196fc0a0d86515d0295813017520f7697b96107
KimDane <kimdane0115@gmail.com>
2025-12-18 04:55:29 +0900

    ✨ feat(app): Ref parameter changed

--- flutter_mobile/lib/feature/1.sign/presentation/providers/sign_repository_provider.dart
+++ flutter_mobile/lib/feature/1.sign/presentation/providers/sign_repository_provider.dart
@@ -6,36 +6,36 @@ import '../../../../index.dart';
 part 'sign_repository_provider.g.dart';
 
 @riverpod
-SupabaseSignApiService supabaseSignApiService (SupabaseSignApiServiceRef ref) {
+SupabaseSignApiService supabaseSignApiService (Ref ref) {
   return SupabaseSignApiServiceImpl();
 }
 
 @Riverpod(keepAlive: true)
-SignRepository signRepository(SignRepositoryRef ref) {
+SignRepository signRepository(Ref ref) {
   final signApiService = ref.read(supabaseSignApiServiceProvider);
   return SignRepositoryImpl(signApiService);
 }
 
 @riverpod
-UserVerify userVerify(UserVerifyRef ref) {
+UserVerify userVerify(Ref ref) {
   final repository = ref.read(signRepositoryProvider);
   return UserVerify(repository);
 }
 
 @riverpod
-AddProfile addProfile(AddProfileRef ref) {
+AddProfile addProfile(Ref ref) {
   final repository = ref.read(signRepositoryProvider);
   return AddProfile(repository);
 }
 
 @riverpod
-DeleteProfile deleteProfile(DeleteProfileRef ref) {
+DeleteProfile deleteProfile(Ref ref) {
   final repository = ref.read(signRepositoryProvider);
   return DeleteProfile(repository);
 }
 
 @riverpod
-GetProfile getProfile(GetProfileRef ref) {
+GetProfile getProfile(Ref ref) {
   final repository = ref.read(signRepositoryProvider);
   return GetProfile(repository);
 }
